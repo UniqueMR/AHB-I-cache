fa339b200c1c68b46ae73c0b7a2f4f759b033496
Runlong Hu <1684525025@qq.com>
2024-12-13 16:55:28 -0500

    code without hope to run

--- src/top.sv
+++ src/top.sv
@@ -21,9 +21,47 @@ module top #(
     // main mem interface
     output [31:0] mem_addr,
     input [127:0] mem_data_in,
-    input mem_ready,
+    Tinput mem_ready,
     output mem_req
 );
+    parameter HIT=1;
+    parameter MISS=0;
+    parameter TRUE=1;
+    parameter FALSE=0;
+    
     cache_entry_t cache_entries [0:CACHE_SIZE-1];
 
+    reg [31:0] data_out_reg;
+    reg [31:0] mem_data_out_reg;
+
+    assign data_out = data_out_reg;
+
+    always_ff @(posedge clk or negedge rst) begin
+    if(~rst)    begin
+        data_out_reg <= 0;
+    end
+    else    begin
+        if(read_en) data_out_reg <= hit ? cache_entries[addr].data : mem_data_out_reg;  
+    end
+    end
+
+    always_comb begin
+        if(mem_req) mem_data_out_reg = mem_data_in[31:0];
+    end
+
+    reg hit_reg;
+    reg mem_req_reg;
+    assign hit = hit_reg;
+    assign mem_req = mem_req_reg;
+    
+    // handle hit or miss
+    always_comb begin
+    hit_reg=MISS;
+    mem_req_reg=FLASE;
+    if(read_en) begin
+        if(cache_entries[addr].valid == HIT)  hit_reg = HIT;
+        else    hit_reg = MISS;           
+    end
+    if(read_en && ~hit) mem_req_reg = TRUE;    
+    end
 endmodule
