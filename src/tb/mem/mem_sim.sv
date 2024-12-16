765928c4910618372ca4be7c4160be7216d2c2fe
unika <runlong@seas.upenn.edu>
2024-12-16 11:46:12 -0500

    resolve non-constant loop bounds

--- src/tb/mem/mem_sim.sv
+++ src/tb/mem/mem_sim.sv
@@ -6,36 +6,35 @@ class memDrive #(
 );
 
 bit [31:0] mem_entries [0:MAIN_MEM_SIZE * 8 / 32 - 1];
-bit [$clog2(MAIN_MEM_SIZE * 8 / 32) - 1 : 0] mem_ptr;
 bit [127:0] mem_read_val;
 bit mem_ready;
 
 function new();
-    for(this.mem_ptr = 0; this.mem_ptr < MAIN_MEM_SIZE * 8 / 32; this.mem_ptr = this.mem_ptr + 1) 
-        this.mem_entries[this.mem_ptr] = $urandom_range(0, 32'hFFFF_FFFF);
-    this.mem_ptr = 0;
+    logic [$clog2(MAIN_MEM_SIZE * 8 / 32) - 1 : 0] init_addr;
+
+    for(init_addr = 0; init_addr < MAIN_MEM_SIZE * 8 / 32; init_addr = init_addr + 1) 
+        this.mem_entries[init_addr] = $urandom_range(0, 32'hFFFF_FFFF);
     this.mem_read_val = 0;
     this.mem_ready = 0;
 endfunction
 
 task automatic mem_read(logic [31:0] mem_addr);
     integer idx = 0;
+    logic [$clog2(MAIN_MEM_SIZE * 8 / 32) - 1 : 0] base_addr;
     #MEM_READ_DELAY;
-for (this.mem_ptr = {mem_addr[$clog2(MAIN_MEM_SIZE * 8 / 32)-1:2], 2'b00};
-     this.mem_ptr <= {mem_addr[$clog2(MAIN_MEM_SIZE * 8 / 32)-1:2], 2'b11};
-     this.mem_ptr = this.mem_ptr + 1) begin
-    this.mem_read_val[idx * 32 + 31 : idx * 32] = this.mem_entries[this.mem_ptr];
-    idx = idx + 1;
-end
+    base_addr = {mem_addr[$clog2(MAIN_MEM_SIZE * 8 / 32)-1:2], 2'b00};
+    for(i = 0; i < 4; i = i + 1)
+       this.mem_read_val[idx * 32 + 31 : idx * 32] = this.mem_entries[base_addr + idx]; 
     this.mem_ready = 1;
     #MEM_READY_HOLD this.mem_ready = 0;
     this.mem_read_val = 0;
 endtask
 
 task automatic mem_write(logic [31:0] mem_addr, logic [31:0] mem_data);
+    logic [$clog2(MAIN_MEM_SIZE * 8 / 32) - 1 : 0] write_addr;
     #MEM_WRITE_DELAY;
-    this.mem_ptr = mem_addr[$clog2(MAIN_MEM_SIZE * 8 / 32)-1:0];
-    this.mem_entries[this.mem_ptr] = mem_data;
+    write_addr = mem_addr[$clog2(MAIN_MEM_SIZE * 8 / 32)-1:0];
+    this.mem_entries[write_addr] = mem_data;
 endtask
 
 endclass
