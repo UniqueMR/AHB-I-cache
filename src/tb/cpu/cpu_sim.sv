2134aa9974de99b69ac76e159e6f1cfa5fa44842
unika <runlong@seas.upenn.edu>
2024-12-22 11:45:43 -0500

    update makefile

--- src/tb/cpu/cpu_sim.sv
+++ src/tb/cpu/cpu_sim.sv
@@ -71,7 +71,10 @@ always_ff @( posedge cpu_intf.hclk or negedge cpu_intf.hrstn) begin : read_reque
     end
 end
 
-assign cpu_intf.haddr = driver_obj.addr;
-assign cpu_intf.hwrite = ~driver_obj.read_en;
+always  begin
+    cpu_intf.haddr = driver_obj.addr;
+    cpu_intf.hwrite = ~driver_obj.read_en;
+    #1;
+end
 
 endmodule
