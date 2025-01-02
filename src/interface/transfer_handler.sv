b4b91ee88a1e3343901b05dbf0a2107c33ad8b97
Runlong Hu <1684525025@qq.com>
2025-01-02 01:39:05 -0500

    try to integrate wrap4 burst into transfer handler

--- src/interface/transfer_handler.sv
+++ src/interface/transfer_handler.sv
@@ -1,3 +1,5 @@
+`include "./src/include/interface_pkg.svh"
+
 module transfer_handler(
     input clk,
     input rstn,
@@ -6,7 +8,9 @@ module transfer_handler(
     input [31:0] hrdata,
     input hready,
     input [31:0] hwdata,
-
+    input [3:0] hburst,
+    
+    output [1:0] htrans,
     output [31:0] read_addr,
     output [31:0] read_data
 );
@@ -15,12 +19,20 @@ reg [31:0] local_addr;
 
 reg [31:0] next_addr;
 
+BURST_TYPES burst_type;
+
+assign burst_type = hburst;
+
+reg [1:0] cnt_wrap4;
+
 always_ff @(posedge clk or negedge rstn) begin
     if(~rstn) begin
         local_addr <= addr;
+        cnt_wrap4 <= 0;
     end
     else begin
         local_addr <= next_addr;
+        cnt_wrap4 <= hready ? (cnt_wrap4 == 2'b11 ? 0 : cnt_wrap4 + 1) : cnt_wrap4;
     end
 end
 
