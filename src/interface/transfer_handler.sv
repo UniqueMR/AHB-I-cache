b4bb7b3e40a3e8069392fd359f8f0a477af1aa93
Runlong Hu <1684525025@qq.com>
2025-01-03 01:56:46 -0500

    add handler for wrap4 burst transfer

--- src/interface/transfer_handler.sv
+++ src/interface/transfer_handler.sv
@@ -15,9 +15,13 @@ module transfer_handler(
     output [31:0] read_data
 );
 
-reg [31:0] local_addr;
+parameter WRAP4_BOUNDARY_MASK = 0xFFFF_FFF0;
 
+reg [31:0] local_addr;
 reg [31:0] next_addr;
+reg [31:0] base_addr;
+reg [31:0] offset_addr;
+reg [31:0] next_offset_addr;
 
 BURST_TYPES burst_type;
 
@@ -29,15 +33,28 @@ always_ff @(posedge clk or negedge rstn) begin
     if(~rstn) begin
         local_addr <= addr;
         cnt_wrap4 <= 0;
+        offset_addr <= 0;
     end
     else begin
         local_addr <= next_addr;
         cnt_wrap4 <= hready ? (cnt_wrap4 == 2'b11 ? 0 : cnt_wrap4 + 1) : cnt_wrap4;
+        offset_addr <= next_offset_addr;
     end
 end
 
 always_comb begin
-    next_addr = hready ? addr : local_addr;
+    next_offset_addr = hready ? ((offset_addr + 4) == 32'h10 ? 0 : offset_addr + 4) : offset_addr;
+    case(hburst)
+        SINGLE: next_addr = hready ? addr : local_addr;
+        WRAP4: begin
+            if(cnt_wrap4 == 2'b11)  next_addr = hready ? addr : local_addr;
+            else if(cnt_wrap4 == 2'b00) begin
+                base_addr = addr & WRAP4_BOUNDARY_MASK;
+                next_addr = hready ? addr : local_addr;
+            end
+            else next_addr = hready ? base_addr + offset_addr : local_addr;
+        end
+    endcase
 end
 
 assign read_data = hrdata;
