e4267a22ad62662f9a6169fe6e8d964266bce0d3
Runlong Hu <1684525025@qq.com>
2025-01-04 23:57:48 -0500

    use next trans out to sync addr

--- src/interface/transfer_handler.sv
+++ src/interface/transfer_handler.sv
@@ -68,8 +68,8 @@ always_comb begin
             end
             else begin
                 next_trans_out = cnt_burst < 2'b11 ? SEQ : IDLE;
-                next_addr = (trans_out == TRANS_TYPES'(SEQ) && hready) ? base_addr + offset_addr : local_addr;
-                next_offset_addr = (trans_out == TRANS_TYPES'(SEQ) && hready) ? ((offset_addr + 4) == 32'h10 ? 0 : offset_addr + 4) : offset_addr;
+                next_addr = (next_trans_out == TRANS_TYPES'(SEQ) && hready) ? base_addr + offset_addr : local_addr;
+                next_offset_addr = (next_trans_out == TRANS_TYPES'(SEQ) && hready) ? ((offset_addr + 4) == 32'h10 ? 0 : offset_addr + 4) : offset_addr;
                 next_base_addr = base_addr;
             end
         end
