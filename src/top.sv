fdcadb6ae46edeaaf6462a55415c1fd61d51a410
Runlong Hu <1684525025@qq.com>
2025-01-08 12:30:11 -0500

    get next mem htrans from upstream

--- src/top.sv
+++ src/top.sv
@@ -129,6 +129,7 @@ always_ff @(posedge downstream_intf.hclk or negedge downstream_intf.hrstn) begin
 end
 
 assign next_downstream_trans = hit ? TRANS_TYPES'(IDLE) : trans_out;
+assign downstream_intf.hburst = TRANS_TYPES'(WRAP4);
 
 always_ff @(posedge downstream_intf.hclk or negedge downstream_intf.hrstn) begin
     if(~downstream_intf.hrstn) downstream_intf.htrans <= 0;
